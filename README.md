c7cff0842a192da2340c4048b9827569b8409cb6
Gautam Kabiraj <51134142+gautamkabiraj96@users.noreply.github.com>
2022-12-07 00:58:15 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# vuejs-3-composition-api-dynamic-years-list-example
-Created with CodeSandbox
+# Vue.js 3 Composition API dynamic years list dropdown example
+
+Learn how to create a list of past years from the current year dynamically in Vue.js 3 Composition API and bind it to a select dropdown menu. [Learn more](https://www.nightprogrammer.com/vue-js/create-years-list-in-select-dropdown-in-vue-3-composition-api-example/).
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const yearsList = ref([])
+const selectedYear = ref(null)
+
+const getYearsList = () => {
+    const startYear = 1990;
+    const endYear = new Date().getFullYear();
+    for (let i = endYear; i >= startYear; i--) {
+        yearsList.value = [...yearsList.value, i]
+    }
+}
+
+onMounted(() => {
+  getYearsList()
+})
+
+</script>
+
+<template>
+<div>
+    <h1>
+      Years list
+    </h1>
+    <p>
+      <select v-model="selectedYear">
+        <option v-for="(year, y) in yearsList" :key="y" :value="year">
+          {{year}}
+        </option>
+      </select>
+    </p>
+    <p>
+      Selected year: {{selectedYear}}
+    </p>
+</div>
+</template>
